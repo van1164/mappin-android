0c7731bf1cc8b826a96fe6dee19758f94f8fb97e
van1164 <vkn123@naver.com>
2023-11-05 12:58:14 +0900

    수정중

--- app/src/main/java/com/example/mapin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/mapin/ui/login/LoginActivity.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.mapin.MainActivity
 import com.example.mapin.R
 import com.example.mapin.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
@@ -26,30 +27,37 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
-
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e(TAG, "카카오계정으로 로그인 실패", error)
+        } else if (token != null) {
+            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         var keyHash = Utility.getKeyHash(this)
-        Log.d("CCCCCCCCCCCCCCCCCCCCCCCC",keyHash)
         setContentView(binding.root)
         val kakaoLoginButton = binding.kakaoLoginButton
         loginViewModel =
             ViewModelProvider(this, LoginViewModelFactory())[LoginViewModel::class.java]
         KakaoSdk.init(this, "ffa56ed9ec1ad5bc1c60999cf73c6708")
         kakaoLoginButton.setOnClickListener{
-            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                if (error != null) {
-                    Log.e(TAG, "로그인 실패", error)
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
                 }
-                else if (token != null) {
-                    Log.i(TAG, "로그인 성공 ${token.accessToken}")
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    intent.flags =
-                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    startActivity(intent)
-                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
 //        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
